a4b182266b302e0cc7d6411b68b5d5fcfd4064c0
nayanthulkar28 <nayanthulkar1@gmail.com>
2022-03-30 16:13:49 +0530

    3d cube

--- src/Mainbody/About/About.js
+++ src/Mainbody/About/About.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './About.css';
+import * as THREE from 'three';
 
-function About() {
+class About extends Component{
+
+    componentDidMount() {
+        const height = this.divElement.clientHeight;
+        const width = this.divElement.clientWidth;
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
+        const renderer = new THREE.WebGL1Renderer();
+        renderer.setSize(width, height);
+        this.mount.appendChild( renderer.domElement );
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({color: 0xBBE1FA});
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        camera.position.z = 5;
+
+        const animate = function() {
+            requestAnimationFrame(animate);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        };
+
+        animate();
+    }
+  render() {
   return (
     <div className = 'about-container' id = 'about-container'>
         <div className = 'about-container-left'>
@@ -11,11 +38,11 @@ function About() {
             </div>
         </div>
         <div className = 'about-container-right'>
-            <div className = 'about-container-right-holder'>
-                wheel
+            <div className = 'about-container-right-holder' ref={ref=>(this.mount=ref)} >
             </div>
         </div>
     </div>
   )
+  }
 }
 export default About;
