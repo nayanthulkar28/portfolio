dd9f845198110efd4c9b69c379356027fe2a193e
nayanthulkar28 <nayanthulkar1@gmail.com>
2022-07-28 18:23:46 +0530

    added responsiveness

--- src/Mainbody/About/About.js
+++ src/Mainbody/About/About.js
@@ -8,131 +8,119 @@ import myFont from '../../resources/Fonts/MyFont.json';
 class About extends Component{
 
     componentDidMount() {
-        const height = 800;
-        const width = 800;
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
         const renderer = new THREE.WebGLRenderer({alpha:true, antialias: true});
-        renderer.setSize(width, height);
         renderer.domElement.id = "canvasWheel";
         this.mount.appendChild( renderer.domElement );
+        let canvasEle = document.querySelector("#canvasWheel");
+        console.log(canvasEle);
+        const height = canvasEle.clientHeight;
+        const width = canvasEle.clientWidth;
+        console.log(width, height);
+        renderer.setSize(width, height);
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, width/height, 0.1, 1000);
         camera.position.z = 130;
 
         const loader = new FontLoader();
 
         var font = loader.parse(myFont)
         const distance = 60;
+        const cornerDistance = distance * Math.cos(Math.PI / 4);
 
         const centerGeometry = new THREE.Object3D();
         scene.add(centerGeometry);
 
         var textMaterial = new THREE.MeshBasicMaterial({color: "#3282B8"});
 
-        var textGeometryHTML = new TextGeometry('HTML', {
-            font: font,
-            size: 3,
-            height: 1,
+        const skills = ["HTML", 
+                        "CSS", 
+                        "JAVASCRIPT", 
+                        "GOLANG", 
+                        "C++", 
+                        "ReactJS", 
+                        "PYTHON", 
+                        "SQL", 
+                        "GIT", 
+                        "GITHUB", 
+                        "PHP", 
+                        "REST", 
+                        "DSA", 
+                        "ALGO"];
+        
+        const skillCords = [[distance, 0, 0], 
+                            [0, distance, 0], 
+                            [0, 0, distance], 
+                            [-distance, 0, 0], 
+                            [0, -distance, 0], 
+                            [0, 0, -distance], 
+                            [cornerDistance, cornerDistance, cornerDistance], 
+                            [-cornerDistance, cornerDistance, cornerDistance], 
+                            [cornerDistance, -cornerDistance, cornerDistance], 
+                            [-cornerDistance, -cornerDistance, cornerDistance], 
+                            [cornerDistance, cornerDistance, -cornerDistance], 
+                            [-cornerDistance, cornerDistance, -cornerDistance], 
+                            [cornerDistance, -cornerDistance, -cornerDistance], 
+                            [-cornerDistance, -cornerDistance, -cornerDistance]];
+                    
+        let skillMesh = [];
+        let objectMesh = [];
+        const totatEle = skillCords.length;
+
+        for(let i = 0 ; i < totatEle ; i ++) {
+            const skill = skills[i];
+            const cords = skillCords[i];
+
+            var geometry = new TextGeometry(skill, {
+                font: font,
+                size: 3,
+                height: 1,
+            });
+
+            var textMesh = new THREE.Mesh(geometry, textMaterial);
+            scene.add(textMesh);
+            skillMesh.push(textMesh);
+
+            const object = new THREE.Object3D();
+            object.position.set(cords[0], cords[1], cords[2]);
+            objectMesh.push(object);
+            centerGeometry.add(object);
+        }
+
+        canvasEle.addEventListener("mousedown", function(e) {
+            setRotateCoordinates(canvasEle, e);
         });
 
-        var textMeshHTML = new THREE.Mesh(textGeometryHTML, textMaterial);
-        scene.add(textMeshHTML);
+        let axisX = 1 / Math.sqrt(2);
+        let axisY = 1 / Math.sqrt(2);
+        let axisZ = 0;
+        let theta = Math.atan(axisY / axisX);
+        let alpha = 0;
+        let angleIncrement = 0;
+        let totalAngleCover = 0;
+        const epsilon = Math.pow(10, -10);
 
-        const objectHTML = new THREE.Object3D();
-        objectHTML.position.set(distance, 0, 0);
-        centerGeometry.add(objectHTML);
-
-        var textGeometryCSS = new TextGeometry('CSS', {
-            font: font,
-            size: 3,
-            height: 1,
-        });
-
-        var textMeshCSS = new THREE.Mesh(textGeometryCSS, textMaterial);
-        scene.add(textMeshCSS);
-
-        const objectCSS = new THREE.Object3D();
-        objectCSS.position.set(0, distance, 0);
-        centerGeometry.add(objectCSS)
-
-        var textGeometryJAVASCRIPT = new TextGeometry('JAVASCRIPT', {
-            font: font,
-            size: 3,
-            height: 1,
-        });
-
-        var textMeshJAVASCRIPT = new THREE.Mesh(textGeometryJAVASCRIPT, textMaterial);
-        scene.add(textMeshJAVASCRIPT);
-
-        const objectJAVASCRIPT = new THREE.Object3D();
-        objectJAVASCRIPT.position.set(0, 0, -distance);
-        centerGeometry.add(objectJAVASCRIPT);
-
-        var textGeometryGOLANG = new TextGeometry('GOLANG', {
-            font: font,
-            size: 3,
-            height: 1,
-        });
-
-        var textMeshGOLANG = new THREE.Mesh(textGeometryGOLANG, textMaterial);
-        scene.add(textMeshGOLANG);
-
-        const objectGOLANG = new THREE.Object3D();
-        objectGOLANG.position.set(-distance, 0, 0);
-        centerGeometry.add(objectGOLANG);
-
-        var textGeometryCPP = new TextGeometry('C++', {
-            font: font,
-            size: 3,
-            height: 1,
-        });
-
-        var textMeshCPP = new THREE.Mesh(textGeometryCPP, textMaterial);
-        scene.add(textMeshCPP);
-
-        const objectCPP = new THREE.Object3D();
-        objectCPP.position.set(0, -distance, 0);
-        centerGeometry.add(objectCPP);
-
-        var textGeometryPYTHON = new TextGeometry('PYTHON', {
-            font: font,
-            size: 3,
-            height: 1,
-        });
-
-        var textMeshPYTHON = new THREE.Mesh(textGeometryPYTHON, textMaterial);
-        scene.add(textMeshPYTHON);
-
-        const objectPYTHON = new THREE.Object3D();
-        objectPYTHON.position.set(0, 0, distance);
-        centerGeometry.add(objectPYTHON);
-
-        // const centerAxis = new THREE.AxesHelper(40);
-        // centerGeometry.add(centerAxis);
-        // const HTMLAxis = new THREE.AxesHelper(10);
-        // textMeshHTML.add(HTMLAxis);
-        // const CSSAxis = new THREE.AxesHelper(10);
-        // textMeshCSS.add(CSSAxis);
-
-        let canvasEle = document.querySelector("#canvasWheel");
-        console.log(canvasEle);
-        canvasEle.addEventListener("mousemove", function(e) {
-            setRotateZ(canvasEle, e);
-        });
-
-        let rotateX = 1 / Math.sqrt(2);
-        let rotateY = 1 / Math.sqrt(2);
-        let rotateZ = 0;
-
-        function setRotateZ(canvas, event) {
+        function setRotateCoordinates(canvas, event) {
             let rect = canvas.getBoundingClientRect();
             let x = (event.clientX - rect.left) - ((rect.right - rect.left) / 2);
             let y = ((rect.bottom - rect.top) / 2) - (event.clientY - rect.top);
+
             // perpendicular vector set
             const mag = Math.sqrt(x * x + y * y);
-            rotateX = -y / mag;
-            rotateY = x / mag;
-            rotateZ = 0;
-            // rotateZ = ((x < 0) ? Math.atan(y / x) : Math.PI + Math.atan(y / x)) - Math.PI;
+            const prevX = axisX;
+            const prevY = axisY;
+            const prevZ = axisZ;
+            axisX = -y / mag;
+            axisY = x / mag;
+            axisZ = 0;
+
+            // calculating angle between two axis using dot product of unit vector
+            alpha = Math.acos(prevX * axisX + prevY * axisY + prevZ * axisZ);
+            angleIncrement = alpha / 50;
+
+            // check if angle is > 180
+            if (Math.abs(axisX - Math.cos(theta + alpha)) >  epsilon || Math.abs(axisY - Math.sin(theta + alpha)) > epsilon) angleIncrement *= -1;
+
+            totalAngleCover = theta + (50 * angleIncrement);
         }
 
         function resizeRendererToDisplaySize(renderer) {
@@ -158,16 +146,17 @@ class About extends Component{
             var emptyQuat = new THREE.Quaternion();
             centerGeometry.setRotationFromQuaternion(emptyQuat);
 
-            const axis = new THREE.Vector3(rotateX, rotateY, rotateZ);
+            const rotX = Math.cos(theta + angleIncrement);
+            const rotY = Math.sin(theta + angleIncrement);
+            const rotZ = 0;
+            theta += angleIncrement;
+            if (Math.abs(theta - totalAngleCover) < epsilon) angleIncrement = 0;
+            const axis = new THREE.Vector3(rotX, rotY, rotZ);
             centerGeometry.rotateOnAxis(axis, time);
-            // centerGeometry.rotation.set(0, time, rotateZ);
 
-            textMeshHTML.position.setFromMatrixPosition(objectHTML.matrixWorld);
-            textMeshCSS.position.setFromMatrixPosition(objectCSS.matrixWorld);
-            textMeshJAVASCRIPT.position.setFromMatrixPosition(objectJAVASCRIPT.matrixWorld);
-            textMeshGOLANG.position.setFromMatrixPosition(objectGOLANG.matrixWorld);
-            textMeshCPP.position.setFromMatrixPosition(objectCPP.matrixWorld);
-            textMeshPYTHON.position.setFromMatrixPosition(objectPYTHON.matrixWorld);
+            for(let i = 0 ; i < totatEle ; i ++) {
+                skillMesh[i].position.setFromMatrixPosition(objectMesh[i].matrixWorld);
+            }
             
             renderer.render(scene, camera);
             requestAnimationFrame(render);
